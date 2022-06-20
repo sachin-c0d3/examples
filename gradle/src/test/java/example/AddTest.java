292d937d0228ca01009d71120e91ee0c0c7f78e9
sachin-c0d3 <101328529+sachin-c0d3@users.noreply.github.com>
2022-06-20 22:25:29 +0530

    Update AddTest.java

--- gradle/src/test/java/example/AddTest.java
+++ gradle/src/test/java/example/AddTest.java
@@ -6,9 +6,11 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class AddTest {
+    @Ignore
     @Test
     public void calc() {
         assertThat(new Add().calc(3,4), is(7));
         assertThat(new Add().calc(14,4), is(18));
+        assertThat(new Add().calc(24,4), is(28));
     }
 }
