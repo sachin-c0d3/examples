66a8710bae1ac271262dee273f8a28a444918ebe
sachin-c0d3 <101328529+sachin-c0d3@users.noreply.github.com>
2022-06-22 10:30:18 +0530

    Update FlakyTest.java

--- gradle/src/test/java/example/FlakyTest.java
+++ gradle/src/test/java/example/FlakyTest.java
@@ -43,3 +43,42 @@ import static org.junit.Assert.*;
        }
     }
 }
+
+public class FlakyTest {
+    @Test
+    public void flaky1() {
+        int max = 50;
+        int min = 41;
+        int range = max - min + 1;
+        int rand = (int)(Math.random() * (range-3)) + min;
+        if (rand < min || rand  > max)  {
+          fail("random number is not between range, flaky test failed");
+        }
+     
+       long timestamp = System.currentTimeMillis() / 1000;
+       if (timestamp > 50) {
+        fail("current time seconds is greater than 50.");
+       }
+    }
+    @Test
+    public void flaky2() {
+       long timestamp = System.currentTimeMillis() / 1000;
+       if (timestamp > 55) {
+        fail("current time seconds is greater than 50.");
+       }
+    }
+  @Test
+    public void flaky3() {
+       long timestamp = System.currentTimeMillis() / 1000;
+       if (timestamp > 57) {
+        fail("current time seconds is greater than 50.");
+       }
+    }
+  @Test
+    public void flaky4() {
+       long timestamp = System.currentTimeMillis() / 1000;
+       if (timestamp > 56) {
+        fail("current time seconds is greater than 50.");
+       }
+    }
+}
